245a6c6e2964ff695dbbf90aa49d7a0d189a4567
korius kussa wenda <koriuswenda@gmail.com>
2024-10-17 18:08:06 +0700

    Install laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+</head>
+
+<body>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus illum magni omnis? Repellat perspiciatis eaque
+        fuga atque itaque facere laudantium molestiae dolorem vel tempore aperiam non voluptatibus ratione quos amet,
+        officia quo omnis nostrum optio ad dignissimos maiores animi veniam. Rem expedita quam unde id consequuntur vel
+        suscipit accusantium eveniet?</p>
+</body>
+
+</html>
